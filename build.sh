8c5b51570fa43346978315d0a0fc287a2c911862
waki285 <suzuneu@suzuneu.com>
2023-11-19 14:13:23 +0100

    a

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir dist
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo build --release --target wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
 wasm-bindgen --target web --out-dir ./dist/ --no-typescript target/wasm32-unknown-unknown/release/bevy-breakout.wasm
