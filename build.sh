8f6404f3f343b5e134c7e0fe302505140e68a0e6
waki285 <suzuneu@suzuneu.com>
2023-11-19 14:14:12 +0100

    install

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 mkdir dist
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
 cargo build --release --target wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
 wasm-bindgen --target web --out-dir ./dist/ --no-typescript target/wasm32-unknown-unknown/release/bevy-breakout.wasm
