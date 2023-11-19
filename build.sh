9536ce0794087d3f4531bc275bddda8302c1a88d
waki285 <suzuneu@suzuneu.com>
2023-11-19 14:12:38 +0100

    a

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 mkdir dist
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo build --release --target wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
 wasm-bindgen --target web --out-dir ./dist/ --no-typescript target/wasm32-unknown-unknown/release/bevy-breakout.wasm
 cp static/index.html dist/index.html
 echo "Build complete"
